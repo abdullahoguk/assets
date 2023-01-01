664bc5c2e49338daf70dffa8e60b563c8bffc440
Abdullah <abdullahoguk@gmail.com>
2023-01-01 15:11:49 +0300

    Create shortlinks.map.js

---
+++ shortlinks.map.js
@@ -0,0 +1,6 @@
+(typeof window !== 'undefined' ? window : global).URLMap = {
+  "/undefined": "https://abdullahoguk.com.tr/",
+  "/": "https://abdullahoguk.com.tr/",
+   "/k8-tumnotlar": "https://docs.google.com/document/d/1ytXmqYCXAzsjWfXO6_725V6vuqrXmKfZJSjcqqbZwZs/edit?usp=sharing",
+
+};
